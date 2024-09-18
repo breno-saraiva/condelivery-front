d59c349bed93ee3cfd510e32d7c573328a6a9780
breno-saraiva <breno.saraiva@livsaude.com.br>
2024-09-18 10:08:07 -0300

    fix: import tailwind

--- src/modules/login/page/index.tsx
+++ src/modules/login/page/index.tsx
@@ -1,5 +1,32 @@
+import { Button } from "@/shared/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/shared/components/ui/card";
+
 function Login() {
-  return <h1>Hello Word</h1>;
+  return (
+    <div>
+      <h1>Hello Word</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+          <Button>Testando</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
 
 export { Login };
