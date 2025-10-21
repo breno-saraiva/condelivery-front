a2ec8ee36050a22fd5ba50b3b1f1665e6eebb90f
breno-saraiva <brenodev.saraiva@gmail.com>
2025-10-21 15:00:57 -0300

    feat: add use token for requisition

--- src/shared/api/https.ts
+++ src/shared/api/https.ts
@@ -1,8 +1,34 @@
 import axios from "axios";
+import toast from "react-hot-toast";
 import { baseURL } from "../config/paths";
 
 function httpClientBuilder() {
   const client = axios.create({ baseURL });
+
+  client.interceptors.request.use((config) => {
+    const token = localStorage.getItem("@token");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  client.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      if (error.response && error.response.status === 401) {
+        localStorage.removeItem("@token");
+        toast.error("Acesso expirado");
+        window.location.href = "/";
+
+        return Promise.reject(error);
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return client;
 }
